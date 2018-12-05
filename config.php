30d574fa71726d05dbbd20b0827ae71f0c144414
ainolshafiq <45637096+ainolshafiq@users.noreply.github.com>
2018-12-06 02:37:18 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
-//masukan config token yg terdapat pada link di packet captur
-// jangan hapus tanda kutip(" ") 
 
-$access_token = "6d8c3c99-b5a1-4b0b-972d-74602c116be4";
+$access_token = "xxxxxxxxxxxxxxxxxxxx";
 
-// kamu bisa membuat banyak config untuk beberapa akun.
+ 
+
+
+
